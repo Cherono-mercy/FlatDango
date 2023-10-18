d94229b3a7ab6bfa6dede6a6feecf51971dc27cb
Cherono-mercy <cheronom142@gmail.com>
2023-10-19 01:01:13 +0800

    Completed

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
+//Declaring global variables
 let films = [];
+let firstFilm = [];
 
-//Fetching first movie details using fetch()
-function firstFilm(){
-    fetch(" http://localhost:3000/films/1")
+
+//Function to fetch all films
+function fetchFilms() {
+  fetch(" http://localhost:3000/films")
     .then((response) => response.json())
     .then((data) => {
-        console.log(data)
-        const sidebar = document.getElementById("sidebar");
-    
-        sidebar.innerHTML = `
-
-        <img src="${data.poster}"/>
-        <div class="title"><h3>Title: ${data.title}</h3></div>
-        <div class="runtime"><p>Runtime: ${data.runtime} mins.</p></div>
-        <div class="showtime"><p>Showtime: ${data.showtime}</p></div>
-        <div class="tickets"><p> Available Tickets: ${data.capacity - data.tickets_sold}</p></div>
-    
-        `
-   
-    })
+      console.log(data);
+      films = data;
+      displayFilmNames();
+    });
 }
-firstFilm();
+fetchFilms();
+
+//Function to display list of films
+function displayFilmNames() {
+  let list = document.getElementById("list");
+  films.forEach((value, key) => {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("filmList");
+    newDiv.innerHTML = `
+         <div onclick="displayFilmInfo" class="title">${value.title}</div>`;
+
+    list.appendChild(newDiv);
+  });
+}
+
+//Getting the first film in the list
+function fetchFirstFilm() {
+  fetch(" http://localhost:3000/films/1")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      firstFilm = data;
+      displayFirstFilm(data);
+    });
+}
+fetchFirstFilm();
+
+
+//Displaying details of the first film
+function displayFirstFilm() {
+  const sidebar = document.getElementById("sidebar");
+
+  sidebar.innerHTML = `
+    <img src="${firstFilm.poster}"/>
+    <div>Title: ${firstFilm.title}</div>
+    <div>Runtime: ${firstFilm.runtime} mins</div>
+    <div>Showtime: ${firstFilm.showtime}</div>
+    <div id="tickets">Available Tickets: ${firstFilm.capacity - firstFilm.tickets_sold}</div>
+    <button>Buy Ticket</button>
+    `;
+}
+
+
